71df5351505dff59c15b457fcc4ca695dc0f8415
bf228-fnf <85364286+bf228-fnf@users.noreply.github.com>
2023-03-18 01:42:45 +0400

    Update mail.php

--- mail.php
+++ mail.php
@@ -12,15 +12,15 @@ $avtor = $_POST['avto'];
 //$mail->SMTPDebug = 3;                               // Enable verbose debug output
 
 $mail->isSMTP();                                      // Set mailer to use SMTP
-$mail->Host = 'smtp.spaceweb.ru';  																							// Specify main and backup SMTP servers
+$mail->Host = 'smtp.mail.ru';  																							// Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'dibil228of@blopsoft.ru'; // Ваш логин от почты с которой будут отправляться письма
+$mail->Username = 'legavorn228@mail.ru'; // Ваш логин от почты с которой будут отправляться письма
 $mail->Password = 'Nightsandytiktok234'; // Ваш пароль от почты с которой будут отправляться письма
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465; // TCP port to connect to / этот порт может отличаться у других провайдеров
 
-$mail->setFrom('dibil228of@blopsoft.ru'); // от кого будет уходить письмо?
-$mail->addAddress('legubrawl@gmail.com');     // Кому будет уходить письмо 
+$mail->setFrom('legavorn228@mail.ru'); // от кого будет уходить письмо?
+$mail->addAddress('dibil228of@blopsoft.ru');     // Кому будет уходить письмо 
 $mail->isHTML(true);                                  // Set email format to HTML
 
 $mail->Subject = 'Приложение $name';
