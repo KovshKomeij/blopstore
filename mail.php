97e818e72c88f51bfb270d72ba9bd20c01d56313
bf228-fnf <85364286+bf228-fnf@users.noreply.github.com>
2023-03-18 20:24:20 +0400

    Add files via upload

--- mail.php
+++ mail.php
@@ -1,35 +1,28 @@
-<?php 
+<?php
 
-require_once('phpmailer/PHPMailerAutoload.php');
-$mail = new PHPMailer;
-$mail->CharSet = 'utf-8';
+/* https://api.telegram.org/bot6053946893:AAH235sW8nXMVjFaHsHzqXKLDsGkEKA_hJc/getUpdates,
+где, XXXXXXXXXXXXXXXXXXXXXXX - токен вашего бота, полученный ранее */
 
 $name = $_POST['name'];
 $desc = $_POST['desc'];
 $webad = $_POST['webpro'];
 $avtor = $_POST['avto'];
+$token = "6053946893:AAH235sW8nXMVjFaHsHzqXKLDsGkEKA_hJc";
+$chat_id = "-982504386";
+$arr = array('Пришла заявка от' => $avtor,
+	'Имя приложения: ' => $name,
+	'Описание приложения: ' => $desc,
+	'Веб-адрес приложения: ' => $webad);
 
-//$mail->SMTPDebug = 3;                               // Enable verbose debug output
+foreach($arr as $key => $value) {
+  $txt .= "<b>".$key."</b> ".$value."%0A";
+};
 
-$mail->isSMTP();                                      // Set mailer to use SMTP
-$mail->Host = 'smtp.yandex.ru';  																							// Specify main and backup SMTP servers
-$mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'blopstore@yandex.ru'; // Ваш логин от почты с которой будут отправляться письма
-$mail->Password = 'bs09081010'; // Ваш пароль от почты с которой будут отправляться письма
-$mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
-$mail->Port = 465; // TCP port to connect to / этот порт может отличаться у других провайдеров
+$sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}","r");
 
-$mail->setFrom('blopstore@yandex.ru'); // от кого будет уходить письмо?
-$mail->addAddress('dibil228of@blopsoft.ru');     // Кому будет уходить письмо 
-$mail->isHTML(true);                                  // Set email format to HTML
-
-$mail->Subject = 'Приложение $name';
-$mail->Body    = 'Привет, я $avtor и я хочу отправить своё приложени в Blopstore, вот инофрмация про моё приложение: Название: $name Описание: $desc веб-адрес приложения: $webad Надеюсь что моё приложение попдёт в BlopStore.';
-$mail->AltBody = '';
-
-if(!$mail->send()) {
-    echo 'Error';
+if ($sendToTelegram) {
+  header('Location: loaded.html');
 } else {
-    header('location: loaded.html');
+  echo "Error";
 }
 ?>
