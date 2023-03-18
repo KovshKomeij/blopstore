e944237c3eab36d762f8e6faad6a91cfa575b7d0
bf228-fnf <85364286+bf228-fnf@users.noreply.github.com>
2023-03-18 15:07:49 +0400

    Update mail.php

--- mail.php
+++ mail.php
@@ -12,14 +12,14 @@ $avtor = $_POST['avto'];
 //$mail->SMTPDebug = 3;                               // Enable verbose debug output
 
 $mail->isSMTP();                                      // Set mailer to use SMTP
-$mail->Host = 'smtp.gmail.com';  																							// Specify main and backup SMTP servers
+$mail->Host = 'smtp.yandex.ru';  																							// Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'blopstore128@gmail.com'; // Ваш логин от почты с которой будут отправляться письма
-$mail->Password = 'iJQKaGkM7d4gSgb'; // Ваш пароль от почты с которой будут отправляться письма
+$mail->Username = 'blopstore@yandex.ru'; // Ваш логин от почты с которой будут отправляться письма
+$mail->Password = 'bs09081010'; // Ваш пароль от почты с которой будут отправляться письма
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465; // TCP port to connect to / этот порт может отличаться у других провайдеров
 
-$mail->setFrom('blopstore128@gmail.com'); // от кого будет уходить письмо?
+$mail->setFrom('blopstore@yandex.ru'); // от кого будет уходить письмо?
 $mail->addAddress('dibil228of@blopsoft.ru');     // Кому будет уходить письмо 
 $mail->isHTML(true);                                  // Set email format to HTML
 
